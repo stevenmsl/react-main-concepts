e293cf88ceabd57e6966cda6a7a27eb78fe9e633
Steven Lin <slin@merchantgrowth.com>
2019-12-09 12:46:58 -0800

    components and props - composing components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo from "./logo.svg";
+import "./App.css";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* components and props - composing components */
+
+//Function can be a valid React component
+//as long as it accepts a single “props” object argument.
+function Welcome(props: any) {
+  return <h1>Hello, {props.name}</h1>;
 }
 
+//extend React.Component class
+interface WelcomeProps {
+  name: string;
+}
+class Welcome2 extends React.Component<WelcomeProps> {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+//using destructuring
+class Welcome3 extends React.Component<{ name: string }> {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+const welcomeSara = <Welcome name="Sara" />;
+const welcomeJoe = <Welcome2 name="Joe" />;
+const welcomeDan = <Welcome3 name="Dan" />;
+
+const App: React.FC = () => {
+  console.log("Type Script!");
+  return (
+    <div className="App">
+      {welcomeSara}
+      {welcomeJoe}
+      {welcomeDan}
+    </div>
+  );
+};
+
+/*
+  const App: React.FC = () => {
+  console.log("Type Script!");
+  return <div className="App"></div>;
+};
+*/
 export default App;
