c20206b9258d97f8d7ba7d4353a4347816e263b8
Steven Lin <slin@merchantgrowth.com>
2019-12-09 15:46:26 -0800

    components and props - extracting components

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,75 @@ import React from "react";
 //import logo from "./logo.svg";
 import "./App.css";
 
+const App: React.FC = () => {
+  console.log("Type Script!");
+  return <div className="App"></div>;
+};
+
+/* components and props - extracting components */
+/*
+//split components into smaller components.
+function Avatar(props: { user: { name: string; avatarUrl: string } }) {
+  //naming props from the component’s own point of view
+  //rather than the context in which it is being used.
+  //All React components must act like pure functions
+  //with respect to their props.
+  return (
+    <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />
+  );
+}
+
+function formatDate(date: Date) {
+  return date.toLocaleDateString();
+}
+function UserInfo(props: { user: { name: string; avatarUrl: string } }) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">{props.user.name}</div>
+    </div>
+  );
+}
+
+function Comment(props: {
+  author: { name: string; avatarUrl: string };
+  text: string;
+  date: Date;
+}) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
+const comment = {
+  date: new Date(),
+  text: "I hope you enjoy learning React!",
+  author: {
+    name: "Hello Kitty",
+    avatarUrl: "https://placekitten.com/g/64/64"
+  }
+};
+
+const App: React.FC = () => {
+  console.log("Type Script!");
+  return (
+    <div className="App">
+      <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author}
+      />
+    </div>
+  );
+};
+*/
 /* components and props - composing components */
 
-//Function can be a valid React component
+/*
+//function can be a valid React component
 //as long as it accepts a single “props” object argument.
 function Welcome(props: any) {
   return <h1>Hello, {props.name}</h1>;
@@ -40,6 +106,7 @@ const App: React.FC = () => {
     </div>
   );
 };
+*/
 
 /*
   const App: React.FC = () => {
