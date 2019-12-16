c0192e531bfcf4e1e7916a30e2e77a2a310ae294
Steven Lin <slin@merchantgrowth.com>
2019-12-16 12:44:30 -0800

    conditional rendering - element variables

--- src/App.tsx
+++ src/App.tsx
@@ -20,11 +20,51 @@ function Greeting(props: { isLoggedIn: boolean }) {
   return <GuestGreeting />;
 }
 
+function LoginButton(props: { onClick: () => void }) {
+  return <button onClick={props.onClick}>Login</button>;
+}
+
+function LogoutButton(props: { onClick: () => void }) {
+  return <button onClick={props.onClick}>Logout</button>;
+}
+
+class LoginControl extends React.Component<{}, { isLoggedIn: boolean }> {
+  constructor(props: {}) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = { isLoggedIn: false };
+  }
+
+  handleLoginClick() {
+    this.setState({ isLoggedIn: true });
+  }
+  handleLogoutClick() {
+    this.setState({ isLoggedIn: false });
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
+}
+
 const App: React.FC = () => {
   console.log("Type Script!");
   return (
     <div className="App">
-      <Greeting isLoggedIn={false} />
+      <LoginControl />
     </div>
   );
 };
