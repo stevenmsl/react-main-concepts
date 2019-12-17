fc66313ae0bbe6efbb9825f970c1b9a730584081
Steven Lin <slin@merchantgrowth.com>
2019-12-17 15:27:35 -0800

    Lists and Keys - correct key usage

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,73 @@ const App: React.FC = () => {
   return <div className="App"></div>;
 };
 
+/* Lists and Keys - correct key usage  */
+
+/*
+function ListItem(props: { value: number }) {
+  //There is no need to specify the key here:
+  return <li>{props.value}</li>;
+}
+function NumberList(props: { numbers: number[] }) {
+  const numbers = props.numbers;
+  // Key should be specified inside the array.
+  const listItems = numbers.map(number => (
+    <ListItem key={number.toString()} value={number} />
+  ));
+  return <ul>{listItems}</ul>;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <NumberList numbers={numbers} />
+    </div>
+  );
+};
+*/
+/* Lists and Keys - Basic List Component */
+/*
+function NumberList(props: { numbers: number[] }) {
+  const numbers = props.numbers;
+  // A “key” is a special string attribute you need to include
+  // when creating lists of elements.
+  // - Keys help React identify which items have changed,
+  //   are added, or are removed.
+  // - Keys should be given to the elements inside the array
+  //   to give the elements a stable identity:
+  const listItems = numbers.map(number => (
+    <li key={number.toString()}>{number}</li>
+  ));
+  return <ul>{listItems}</ul>;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <NumberList numbers={numbers} />
+    </div>
+  );
+};
+*/
+
+/* Lists and Keys - Rendering Multiple Components */
+/*
+const numbers = [1, 2, 3, 4, 5];
+//JSX.Element[]
+const listItems = numbers.map(number => <li>{number}</li>);
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <ul>{listItems}</ul>
+    </div>
+  );
+};
+*/
+
 /* Conditional rendering - Preventing Component from Rendering */
 
 /*
